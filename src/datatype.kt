a8c16ad8db38b41cec3e077a7244b0ac0ef4f760
shirlingggg <shirleenkabunyi@gmail.com>
2025-07-15 11:35:20 +0300

    initial

---
+++ src/datatype.kt
@@ -0,0 +1,18 @@
+fun main() {
+
+    var num : Int = -5 //int
+    var mynumber : Float = 56.7F //float
+    var temperature : Double = 45.5 //double
+    var grade : Char = 'A' //char
+    var language : String= "Kotlin" //string
+    var isMammal : Boolean = true //boolean
+
+    println(num)
+    println(mynumber)
+    println(temperature)
+    println(grade)
+    println(language)
+    println(isMammal)
+
+
+}
---
+++ src/variables.kt
@@ -0,0 +1,16 @@
+//Variable is a container used to store a value
+
+fun main() {
+    var number = 56
+    number=100
+    println(number)
+
+
+    val pi=3.14
+    println(pi)
+
+    var name : String
+    name = "James"
+    println(name)
+
+}
